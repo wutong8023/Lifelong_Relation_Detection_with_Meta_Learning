05b5e6bda0504190718f8bef72156b73c514dc34
wutong8023 <14964542+wutong8023@users.noreply.github.com>
2020-03-20 17:18:58 +1100

    dimension 300 -> 200

--- train.py
+++ train.py
@@ -3,15 +3,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import quadprog
 import random
 import time
 
 from data import gen_data, read_origin_relation
 from model import SimilarityModel
-from utils import process_testing_samples, process_samples, ranking_sequence,\
-    copy_grad_data, get_grad_params
-from evaluate import evaluate_model
+from utils import process_testing_samples, process_samples, ranking_sequence
+
 from config import CONFIG as conf
 
 embedding_dim = conf['embedding_dim']
@@ -31,7 +29,7 @@ def feed_samples(model, samples, loss_function, all_relations, device,
         samples, all_relations, device)
     ranked_questions, alignment_question_indexs = \
         ranking_sequence(questions)
-    ranked_relations, alignment_relation_indexs =\
+    ranked_relations, alignment_relation_indexs = \
         ranking_sequence(relations)
     question_lengths = [len(question) for question in ranked_questions]
     relation_lengths = [len(relation) for relation in ranked_relations]
@@ -54,18 +52,19 @@ def feed_samples(model, samples, loss_function, all_relations, device,
     start_index = 0
     for length in relation_set_lengths:
         pos_index.append(start_index)
-        pos_scores.append(all_scores[start_index].expand(length-1))
-        neg_scores.append(all_scores[start_index+1:start_index+length])
+        pos_scores.append(all_scores[start_index].expand(length - 1))
+        neg_scores.append(all_scores[start_index + 1:start_index + length])
         start_index += length
     pos_scores = torch.cat(pos_scores)
     neg_scores = torch.cat(neg_scores)
     alignment_model_criterion = nn.MSELoss()
 
     loss = loss_function(pos_scores, neg_scores,
-                         torch.ones(sum(relation_set_lengths)-
+                         torch.ones(sum(relation_set_lengths) -
                                     len(relation_set_lengths)))
     loss.backward()
     return all_scores, loss
+
 
 def train(training_data, valid_data, vocabulary, embedding_dim, hidden_dim,
           device, batch_size, lr, embedding, all_relations,
@@ -81,8 +80,8 @@ def train(training_data, valid_data, vocabulary, embedding_dim, hidden_dim,
     best_acc = 0
     memory_index = 0
     for epoch_i in range(epoch):
-        for i in range((len(training_data)-1)//batch_size+1):
-            samples = training_data[i*batch_size:(i+1)*batch_size]
+        for i in range((len(training_data) - 1) // batch_size + 1):
+            samples = training_data[i * batch_size:(i + 1) * batch_size]
             seed_rels = []
             for item in samples:
                 if item[0] not in seed_rels:
@@ -91,14 +90,14 @@ def train(training_data, valid_data, vocabulary, embedding_dim, hidden_dim,
             if len(memory_data) > 0:
                 all_seen_data = []
                 for this_memory in memory_data:
-                    all_seen_data+=this_memory
+                    all_seen_data += this_memory
                 memory_batch = memory_data[memory_index]
                 scores, loss = feed_samples(model, memory_batch,
                                             loss_function,
                                             all_relations, device,
                                             alignment_model)
                 optimizer.step()
-                memory_index = (memory_index+1)%len(memory_data)
+                memory_index = (memory_index + 1) % len(memory_data)
             scores, loss = feed_samples(model, samples, loss_function,
                                         all_relations, device, alignment_model)
             optimizer.step()
@@ -106,9 +105,10 @@ def train(training_data, valid_data, vocabulary, embedding_dim, hidden_dim,
             del loss
     return model
 
+
 if __name__ == '__main__':
     training_data, testing_data, valid_data, all_relations, vocabulary, \
-        embedding=gen_data()
+    embedding = gen_data()
     train(training_data, valid_data, vocabulary, embedding_dim, hidden_dim,
           device, batch_size, lr, model_path, embedding, all_relations,
           model=None, epoch=100)
